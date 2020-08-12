e5c228243ac433e0d06d830f52b613a3be8274c8
njmit123 <nileshnj993@gmail.com>
2020-08-12 21:28:47 +0530

    Tokenization example

--- tokenization.py
+++ tokenization.py
@@ -1,2 +1,5 @@
-from nltk.corpus import brown
-print(brown)
+from nltk.tokenize import sent_tokenize,word_tokenize
+text="Hello there, how are you? I am Nilesh." # we would usually split by spaces for words and punctuation for sentences but cases like Mr.Smith can arise that are ambiguous
+# NLTK solves these issues with ease as compared to using regexp
+print(word_tokenize(text)) # split into words
+print(sent_tokenize(text)) # split into sentences
