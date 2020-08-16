f65a4e25f50da8caad97bdbd5fbd28f41a197827
njmit123 <nileshnj993@gmail.com>
2020-08-17 00:20:31 +0530

    Added Heartfulness Feedback form

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,6 +1,7 @@
 import GetOldTweets3 as got
 
 def get_tweets():
+    
     tweetCriteria = got.manager.TweetCriteria().setQuerySearch('corona virus')\
                                            .setSince("2019-08-01")\
                                            .setUntil("2020-02-28")\
