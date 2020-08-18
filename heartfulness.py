98deed27cd19fc1ca86db0d4899e7c76c3ec4e54
njmit123 <nileshnj993@gmail.com>
2020-08-19 00:53:07 +0530

    Twitter sentiment analysis mini project

---
+++ heartfulness.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+heartfulness_df = pd.read_csv('/home/njeyepatch/Computer Science/Machine Learning/NLP/NLTK_practice/Participants - Virtual Heartfulness Feedback Form  (Responses).csv')
+print(heartfulness_df['Feedback / Suggestion you would like to share with us.'])
